18cc634575b037496889251341df72f466be7b6d
Donald Doricent <ddoricent97@gmail.com>
2025-09-15 23:46:52 -0400

    simple blinker program.

---
+++ LED_Blinkers/LED_Blinkers.ino
@@ -0,0 +1,34 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT); // LED 1 control pin is set up as an output
+  pinMode(3, OUTPUT); // LED 2 control pin is set up as an output
+  pinMode(4, OUTPUT); // LED 3 control pin is set up as an output
+  pinMode(5, OUTPUT); // LED 4 control pin is set up as an output
+  pinMode(6, OUTPUT); // LED 5 control pin is set up as an output
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2, HIGH); // turn LED 1 on
+  delay(500); // wait for 0.5 seconds
+  digitalWrite(2, LOW); // turn LED 1 off
+
+  digitalWrite(3, HIGH); // turn LED 2 on
+  delay(500); // wait for 0.5 seconds
+  digitalWrite(3, LOW); // turn LED 2 off
+
+  digitalWrite(4, HIGH); // turn LED 3 on
+  delay(500); // wait for 0.5 seconds
+  digitalWrite(4, LOW); // turn LED 3 off
+
+  digitalWrite(5, HIGH); // turn LED 4 on
+  delay(500); // wait for 0.5 seconds
+  digitalWrite(5, LOW); // turn LED 4 off
+
+  digitalWrite(6, HIGH); // turn LED 5 on
+  delay(500); // wait for 0.5 seconds
+  digitalWrite(6, LOW); // turn LED 5 off
+
+
+}
